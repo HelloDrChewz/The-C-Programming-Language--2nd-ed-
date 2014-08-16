8227108577f6a9d9229fc9b64992fa5612f63a70
HelloDrChewz <HelloDrChewz@yahoo.com>
2014-08-16 16:20:23 -0400

    Update 1_15.c

--- 1_15.c
+++ 1_15.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+float cfc(float fahr);
+
+/*float ccf(float fahr);*/
+
+main()
+{
+  int i;
+  for (i=0;i<=15;++i)
+    printf("%3f\t%3f\n", i*20.0, cfc(i*20.0));
+  return 0;
+}
+
+float cfc(float fahr)
+{
+  return (5.0/9.0)*(fahr-32.0);
+}
